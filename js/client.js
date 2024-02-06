d0948d229375f00caa84cc2a51f79de574b33e2c
Gabe261 <gabed.siewert@hotmail.com>
2024-02-06 13:50:21 -0500

    First Commit

---
+++ js/client.js
@@ -0,0 +1,19 @@
+/**
+ * File Name : client.js
+ *
+ * Revision History:
+ *       Gabriel Siewert, 2024-02-06 : Created
+ */
+
+const btnCalculate_click = () => {
+    const number1 = Number(document.querySelector("#txtFirstNumber").value);
+    const number2 = Number(document.querySelector("#txtSecondNumber").value);
+
+    const output = document.querySelector("#lblResult");
+
+    add(number1, number2, (data) => {
+        output.innerHTML += <h3>The addition is : ${data}</h3>
+    }, (e) => {
+        output.innerHTML += e;
+    });
+}
---
+++ js/server.js
@@ -0,0 +1,31 @@
+/**
+ * File Name : server.js
+ *
+ * Revision History:
+ *       Gabriel Siewert, 2024-02-06 : Created
+ */
+
+const add = (num1, num2, success, fail) => {
+    setTimeout( () => {
+        if(num1 > 10 || num2 > 10){
+            fail("numbers are too big!");
+        } else {
+            const result = num1 + num2;
+            success(`The result is : ${result}`);
+        }
+    }, 2000);
+}
+
+const factorial = (num, success, fail) => {
+    setTimeout( () => {
+        let result = 1;
+        if(number < 0){
+            fail("Number is too small!");
+        } else {
+            for(let i = 1; i <= num; i++){
+                result = result * i;
+            }
+            success(`The result is : ${result}`);
+        }
+    }, 2000);
+}
